ddf0c6efdd0065147dce981e56b3a73abccc085f
sanmiquile <smquinterol@uqvirtual.edu.co>
2023-10-09 12:19:10 -0500

    Se elimina lo de cucumber y se crea la Page y test de Question

---
+++ src/main/java/Page/QuestionPage.java
@@ -0,0 +1,34 @@
+package Page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class QuestionPage extends HomeBasePage{
+    By codQuestionLocator = By.id("formulario:j_idt705");
+    By questionLocator = By.id("formulario:j_idt706");
+
+    By checkboxLocator = By.xpath("//div[@class='ui-chkbox-box ui-widget ui-corner-all ui-state-default'" +
+                    "/span[@class='ui-chkbox-icon ui-icon ui-icon-blank ui-c']");
+    By confirmQuestionBtnLocator = By.xpath("//span[@class='ui-button-text ui-c' and text()='Aceptar']");
+    By messageRegisterQ = By.xpath("//div[@class='ui-growl-message']/span[@class='ui-growl-title']");
+
+
+    public QuestionPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void questionPage(String codigoQ, String question) throws InterruptedException{
+        type(codigoQ, codQuestionLocator);
+        type(question, questionLocator);
+        click(checkboxLocator);
+        click(confirmQuestionBtnLocator);
+    }
+    public String registerMessageQ(){
+        // WaitforVisibleElement
+        getEwait().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(messageRegisterQ));
+        // Se manda el elemento para obtener el texto
+        return getText(messageRegisterQ);
+
+    }
+}
